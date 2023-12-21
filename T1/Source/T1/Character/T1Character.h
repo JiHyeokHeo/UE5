dd733e6952bfb4c75040861191512128c73246f0
JiHyeok Heo <vndj1212@gmail.com>
2023-12-22 07:57:01 +0900

    Input Legacy Version & New Version

--- T1/Source/T1/Character/T1Character.h
+++ T1/Source/T1/Character/T1Character.h
@@ -26,8 +26,8 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
-	UFUNCTION(BlueprintCallable)
-	void TestFunc() {};
+	//UFUNCTION(BlueprintCallable)
+	//void TestFunc() {};
 
 public:
 	UPROPERTY(EditAnywhere, Category=Character)
